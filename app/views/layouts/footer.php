14d5239634b0edbca9c0fc195b6c34ff12cada73
Anthony Dorly Huilahuaña Chata <1677406@senati.pe>
2026-06-04 09:05:41 -0500

    scripts actualizados

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,6 +1,8 @@
-</div> <!-- fin main-content -->
+</main> <!-- fin main-content -->
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="/PROYECTO-tecnosoluciones-sgp/public/assets/js/main.js"></script>
 </body>
 </html>
